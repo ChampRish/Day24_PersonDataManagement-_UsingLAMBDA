151afc749ba286de8f2c5e90482530cd87204365
Rishabh gupta <99132919+ChampRish@users.noreply.github.com>
2022-04-07 14:23:16 +0530

    Retrieve top 2 records from the Lessthan60

--- Person.cs
+++ Person.cs
@@ -8,30 +8,18 @@ namespace Day24_Person_data_management
 {
     public class Person
     {
-        Console.WriteLine(" Welcome to person data management program.");
-            PersonData data = new PersonData();
-            data.NewList();
+       public string SSN;
+        public string name;
+        public string address;
+        public int age;
 
-            Console.WriteLine("\n 2] Retrieve top 2 records from the list for age is less than 60 : ");
-            data.ListTop2RecordsAgeLessThan60();
-
-            Console.WriteLine("\n 3] Retrieve all record from the list for age between 13 to 18 : ");
-            data.RecordsBetweenGivenAge();
-
-            data.CalculateAverageAge();
-
-            Console.WriteLine("\n 5] Searching for value in list :");
-            Console.Write("\n Enter a value to search : ");
-            string value = Console.ReadLine();
-            data.SearchValue(value);
-
-            Console.WriteLine("\n 6] Skip record from the list for age is less than 60");
-            data.SkipRecords();
-
-            Console.WriteLine("\n 7] Removing a value from list :");
-            Console.Write("\n Enter a value to remove : ");
-            string delValue = Console.ReadLine();
-            data.RemoveRecord(delValue);
+        public Person(string SSN, string name, string address, int age)
+        {
+            this.SSN = SSN;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
         
     }
 }
