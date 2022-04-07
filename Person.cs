ea26f65334ce5db12960eded37f07b402933e0ff
RISHABH <gguptarishabh82@gmail.com>
2022-04-07 14:48:15 +0530

    Searching a term in list implemented

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24_Person_data_management
+{
+    public class Person
+    {
+        public string SSN;
+        public string name;
+        public string address;
+        public int age;
+
+        public Person(string SSN, string name, string address, int age)
+        {
+            this.SSN = SSN;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Day24_Person_data_management
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Welcome to person data management program.");
+            PersonData data = new PersonData();
+            data.NewList();
+
+            Console.WriteLine("\n 1] Retrieve top 2 records from the list for age is less than 60 : ");
+            data.ListTop2RecordsAgeLessThan60();
+
+            Console.WriteLine("\n 2] Retrieve all record from the list for age between 13 to 18 : ");
+            data.RecordsBetweenGivenAge();
+
+            data.CalculateAverageAge();
+
+            Console.WriteLine(" 4] Searching for value in list :");
+            Console.Write("\n Enter a value to search : ");
+            string value = Console.ReadLine();
+            data.SearchValue(value);
         }
     }
 }
