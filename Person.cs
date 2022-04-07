e9aad439732ad9dad2d2848e02ea6f98b320ddd6
RISHABH <gguptarishabh82@gmail.com>
2022-04-07 14:03:26 +0530

    Retrieve top 2 records from the less than 60

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24_Person_data_management
+{
+    internal class Person
+    {
+    }
+}
